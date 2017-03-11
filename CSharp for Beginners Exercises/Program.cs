a57e2c5dc2c9acda2a4b9183fbaaec6a36d3d191
christianv07 <christianvasquezmelo@gmail.com>
2017-03-11 18:12:14 -0400

    Add third exercise

--- CSharp for Beginners Exercises/Program.cs
+++ CSharp for Beginners Exercises/Program.cs
@@ -7,11 +7,16 @@ namespace CSharp_for_Beginners_Exercises
     {
         private static void Main(string[] args)
         {
-            var firstExercise = new FirstExercise();
-            firstExercise.Run();
+//            var firstExercise = new FirstExercise();
+//            firstExercise.Run();
 
-            var secondExercise = new SecondExercise();
-            secondExercise.Run();
+//            var secondExercise = new SecondExercise();
+//            secondExercise.Run();
+
+            var thirdExercise = new ThirdExercise();
+            thirdExercise.Run();
+
+            Console.ReadKey();
         }
     }
 }
