b6dc05736666a4fd443863ea63818b2c8451e861
christianv07 <christianvasquezmelo@gmail.com>
2017-03-11 15:27:45 -0400

    init

--- CSharp for Beginners Exercises/Program.cs
+++ CSharp for Beginners Exercises/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace CSharp_for_Beginners_Exercises
+﻿using System;
+
+namespace CSharp_for_Beginners_Exercises
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+            // Display the count on the console.
+
 
         }
     }
