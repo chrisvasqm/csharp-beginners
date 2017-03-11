00cb75d231faf5186de9a9d4fa37596509821947
christianv07 <christianvasquezmelo@gmail.com>
2017-03-11 15:42:06 -0400

    Add FirstExercise file and moved the logic to another class

---
+++ CSharp for Beginners Exercises/FirstExercise.cs
@@ -0,0 +1,28 @@
+﻿
+// Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+// Display the count on the console.
+
+using System;
+
+namespace CSharp_for_Beginners_Exercises
+{
+    internal class FirstExercise
+    {
+        public void Run()
+        {
+            Console.WriteLine(CountNumbersDivisibleByThree(1, 100));
+        }
+
+        public int CountNumbersDivisibleByThree(int start, int end)
+        {
+            var count = 0;
+            for (var i = start; i <= end; i++)
+            {
+                if (i % 3 == 0)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
--- CSharp for Beginners Exercises/Program.cs
+++ CSharp for Beginners Exercises/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-
+﻿
 namespace CSharp_for_Beginners_Exercises
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            // Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
-            // Display the count on the console.
-
-
+            var firstExercise = new FirstExercise();
+            firstExercise.Run();
         }
     }
 }
