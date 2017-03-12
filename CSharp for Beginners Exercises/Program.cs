ce18d56c66a3c0ae29940dc9ee1473212ec8b192
christianv07 <christianvasquezmelo@gmail.com>
2017-03-12 00:51:05 -0400

    Add second exercise to the conditional folder

--- CSharp for Beginners Exercises/Program.cs
+++ CSharp for Beginners Exercises/Program.cs
@@ -7,23 +7,8 @@ namespace CSharp_for_Beginners_Exercises
     {
         private static void Main(string[] args)
         {
-//            var firstExercise = new FirstExercise();
-//            firstExercise.Run();
-
-//            var secondExercise = new SecondExercise();
-//            secondExercise.Run();
-
-//            var thirdExercise = new ThirdExercise();
-//            thirdExercise.Run();
-
-//            var fourthExercise = new FourthExercise();
-//            fourthExercise.Run();
-
-//            var fifthExercise = new FifthExercise();
-//            fifthExercise.Run();
-
-            var firstExercise = new FirstExercise();
-            firstExercise.Run();
+            var secondExercise = new SecondExercise();
+            secondExercise.Run();
 
             Console.ReadKey();
         }
