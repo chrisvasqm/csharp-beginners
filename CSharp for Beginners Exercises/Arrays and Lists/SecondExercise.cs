a3317561b76cf71baac7aa0a971ea4a0495cf312
christianv07 <christianvasquezmelo@gmail.com>
2017-04-02 09:47:37 -0400

    Add second exercise for Arrays and Lists

---
+++ CSharp for Beginners Exercises/Arrays and Lists/SecondExercise.cs
@@ -0,0 +1,42 @@
+﻿// 2- Write a program and ask the user to enter their name.Use an array to reverse the name and then store the result in 
+// a new string. Display the reversed name on the console.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp_for_Beginners_Exercises.Arrays_and_Lists
+{
+    internal class SecondExercise
+    {
+        public void Run()
+        {
+            ReverseName();
+        }
+
+        public void ReverseName()
+        {
+            Console.WriteLine("Enter the name you wish to reverse: ");
+            var name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Please provide a name");
+            }
+            else
+            {
+                var list = new List<char>();
+                foreach (var character in name)
+                {
+                    list.Add(character);
+                }
+
+                list.Reverse();
+
+                var reversedName = string.Join("", list.ToArray());
+
+                Console.WriteLine("The name {0} gets reversed to {1}", name, reversedName);
+            }
+        }
+    }
+}
--- CSharp for Beginners Exercises/Program.cs
+++ CSharp for Beginners Exercises/Program.cs
@@ -7,8 +7,8 @@ namespace CSharp_for_Beginners_Exercises
     {
         private static void Main(string[] args)
         {
-            var firstExercise = new FirstExercise();
-            firstExercise.Run();
+            var secondExercise = new SecondExercise();
+            secondExercise.Run();
 
             Console.ReadKey();
         }
