c5337d7d9d1083d0961bd43179af5071bed1b665
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-11-30 20:04:07 -0400

    Refactor to add null checks

--- CSharp for Beginners Exercises/Control Flow/MaxTwoIntegers.cs
+++ CSharp for Beginners Exercises/Control Flow/MaxTwoIntegers.cs
@@ -1,18 +1,17 @@
-﻿// Write a program which takes two numbers from the console and displays the maximum of the two.
-
-using System;
+﻿using System;
 using System.Linq;
 
 namespace CSharp_for_Beginners_Exercises.Control_Flow
 {
+    // Write a program which takes two numbers from the console and displays the maximum of the two.
     internal static class MaxTwoIntegers
     {
         public static void Run()
         {
             Console.WriteLine("Type in 2 numbers (separated by space)to be compared to know which one is the highest");
             var numbers = Console.ReadLine()?.Split(' ').Select(int.Parse).ToArray();
-            var firstNumber = numbers.First();
-            var secondNumber = numbers.Last();
+            var firstNumber = Convert.ToInt32(numbers?.First());
+            var secondNumber = Convert.ToInt32(numbers?.Last());
             
             if (firstNumber == secondNumber)
                 Console.WriteLine("The provided values are the same");
