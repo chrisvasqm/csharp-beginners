b08a21676e2eb6936e0c9e522e8d5749690adc47
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-02 08:02:16 -0400

    Refactor Loops FirstExercise

---
+++ CSharp for Beginners Exercises/Control Flow/DivisibleByThree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp_for_Beginners_Exercises.Control_Flow
+{
+    /*
+     * Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+     * Display the count on the console.
+     */
+    internal static class DivisibleByThree
+    {
+        public static void Run()
+        {
+            Console.WriteLine("How many numbers are divisible by 3? {0}", CountNumbersDivisibleByThree());
+        }
+
+        private static int CountNumbersDivisibleByThree()
+        {
+            var count = 0;
+            for (var i = 1; i <= 100; i++)
+                if (i % 3 == 0) count++;
+
+            return count;
+        }
+    }
+}
