089341e52b0aeb4ef5af039c09220bf040b69b64
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-02 16:15:28 -0400

    Refactor FourthExercise from Arrays and Lists

---
+++ CSharp for Beginners Exercises/Arrays and Lists/DisplayUniqueNumbersWithoutDuplicates.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp_for_Beginners_Exercises.Arrays_and_Lists
+{
+    /*
+     * Write a program and ask the user to continously enter a number or type "Quite"
+     * to exit. The list of numbers may include duplicates. Display the unique numbers
+     * that the user has entered.
+     */
+    internal static class DisplayUniqueNumbersWithoutDuplicates
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Enter whatever number you like or 'quit' to exit out and view the results");
+
+            var numbers = new List<int>();
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input != null && input.ToLower() == "quit")
+                {
+                    var uniqueNumbers = GetUniqueNumbersList(numbers);
+                    uniqueNumbers.Sort();
+                    uniqueNumbers.ForEach(Console.WriteLine);
+                }
+                else
+                {
+                    numbers.Add(Convert.ToInt32(input));
+                }
+            }
+        }
+
+        private static List<int> GetUniqueNumbersList(IEnumerable<int> numbers)
+        {
+            return numbers.GroupBy(number => number)
+                .Where(number => number.Count() == 1)
+                .SelectMany(number => number)
+                .ToList();
+        }
+    }
+}
