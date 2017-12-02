4f3bbedfd948b6bb304830999c8c43b1f1877c6a
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-02 08:27:12 -0400

    Refactor SecondExercise from Loops

---
+++ CSharp for Beginners Exercises/Control Flow/SumAllEnteredNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharp_for_Beginners_Exercises.Control_Flow
+{
+    /*
+     * Write a program and continously ask the user to enter a number or "ok" to exit.
+     * Calculate the sum of all the previously entered numbers and siplay it on the console.
+     */
+    internal static class SumAllEnteredNumbers
+    {
+        public static void Run()
+        {
+            var sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter a number: ");
+                var input = Console.ReadLine();
+
+                if (input == "ok")
+                    break;
+
+                var number = Convert.ToInt32(input);
+
+                sum += number;
+            }
+            
+            Console.WriteLine("Sum of all the numbers is: {0}", sum);
+        }
+    }
+}
