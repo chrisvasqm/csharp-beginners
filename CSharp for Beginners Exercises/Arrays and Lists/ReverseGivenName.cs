a2728573470a97d0288182b1ef5eb8357c44bdf2
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-02 11:03:15 -0400

    Refactor SecondExercise from Arrays and Lists folder

---
+++ CSharp for Beginners Exercises/Arrays and Lists/ReverseGivenName.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp_for_Beginners_Exercises.Arrays_and_Lists
+{
+    /*
+     * Write a program and ask the user to enter their name.
+     * Use an array to reverse the name and then store the result in a new String.
+     *
+     * Display the reversed name on the console.
+     */
+    internal static class ReverseGivenName
+    {
+        public static void Run()
+        {
+            var name = AskForName();
+            var reversedName = GetReversedName(name);
+            
+            Console.WriteLine("The name {0} gets reversed to {1}", name, reversedName);
+        }
+
+        private static string GetReversedName(string name)
+        {
+            var list = name.ToList();
+            list.Reverse();
+
+            return string.Join("", list.ToArray());
+        }
+
+        private static string AskForName()
+        {
+            Console.WriteLine("Enter the name you wish to reverse: ");
+            
+            return Console.ReadLine();
+        }
+    }
+}
